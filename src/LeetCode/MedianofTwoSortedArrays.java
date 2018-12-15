e567ebaecfd1c18fc2eca18da7ed3cb0ec47a589
Junrui Gong <jgong8@hawk.iit.edu>
2018-12-15 12:40:01 +0800

    Median of Two Sorted Arrays

---
+++ src/LeetCode/MedianofTwoSortedArrays.java
@@ -0,0 +1,76 @@
+package LeetCode;
+
+import org.junit.Test;
+
+/**
+ * Leetcode-4-Median of Two Sorted Arrays
+ *
+ * There are two sorted arrays nums1 and nums2 of size m and n respectively.
+ *
+ * Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
+ *
+ * You may assume nums1 and nums2 cannot be both empty.
+ *
+ * Example 1:
+ * nums1 = [1, 3]
+ * nums2 = [2]
+ *
+ * The median is 2.0
+ *
+ * Example 2:
+ * nums1 = [1, 2]
+ * nums2 = [3, 4]
+ *
+ * The median is (2 + 3)/2 = 2.5
+ *
+ *
+ * 参考：https://blog.csdn.net/chen_xinjia/article/details/69258706
+ * */
+public class MedianofTwoSortedArrays {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        if (nums1.length > nums2.length) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
+
+        int len = nums1.length + nums2.length;
+        int cut1 = 0;
+        int cut2 = 0;
+        int cutL = 0;
+        int cutR = nums1.length;
+
+        while (cut1 <= nums1.length) {
+            cut1 = (cutR - cutL) / 2 + cutL;
+            cut2 = len / 2 - cut1;
+            double L1 = (cut1 == 0) ? Integer.MIN_VALUE : nums1[cut1 - 1];
+            double L2 = (cut2 == 0) ? Integer.MIN_VALUE : nums2[cut2 - 1];
+            double R1 = (cut1 == nums1.length) ? Integer.MAX_VALUE : nums1[cut1];
+            double R2 = (cut2 == nums2.length) ? Integer.MAX_VALUE : nums2[cut2];
+
+            if (L1 > R2) {
+                cutR = cut1 - 1;
+            } else if (L2 > R1) {
+                cutL = cut1 + 1;
+            } else {
+                if (len % 2 == 0) {  // 偶数情况
+                    L1 = L1 > L2 ? L1 : L2;
+                    R1 = R1 < R2 ? R1 : R2;
+                    return (L1 + R1) / 2;
+                } else {
+                    // 奇数情况
+                    R1 = (R1 < R2) ? R1 : R2;
+                    return R1;
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    @Test
+    public void Test(){
+        int[] nums1={1,2};
+        int[] nums2={3,4};
+        System.out.println(findMedianSortedArrays(nums1,nums2));
+
+    }
+}
