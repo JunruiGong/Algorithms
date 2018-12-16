bf51e0e5565531cd0323ed791f092e08ce1659d1
Junrui Gong <jgong8@hawk.iit.edu>
2018-12-16 23:07:26 +0800

    Maximum Subarray

---
+++ src/LeetCode/MaximumSubarray.java
@@ -0,0 +1,41 @@
+package LeetCode;
+
+import org.junit.Test;
+
+/**
+ * Leetcode-53-Maximum Subarray
+ *
+ * Given an integer array nums,
+ * find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+ *
+ * Example:
+ *
+ * Input: [-2,1,-3,4,-1,2,1,-5,4],
+ * Output: 6
+ * Explanation: [4,-1,2,1] has the largest sum = 6.
+ *
+ * Follow up:
+ * If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
+ * */
+public class MaximumSubarray {
+    public int maxSubArray(int[] nums) {
+        int[] dp = new int[nums.length];
+
+        dp[0] = nums[0];
+
+        int result = dp[0];
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = nums[i] + (dp[i - 1] < 0 ? 0 : dp[i - 1]);
+            result = Math.max(result, dp[i]);
+        }
+
+        return result;
+    }
+
+    @Test
+    public void Test() {
+        int[] array = {1, 3, -4, 2};
+        System.out.println(maxSubArray(array));
+
+    }
+}
