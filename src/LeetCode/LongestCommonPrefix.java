ee99a7e065014be7504726ded40f5dcc635485fc
Junrui Gong <jgong8@hawk.iit.edu>
2018-12-20 12:11:50 +0800

    Longest Common Prefix

---
+++ src/LeetCode/LongestCommonPrefix.java
@@ -0,0 +1,38 @@
+package LeetCode;
+
+/**
+ * Leetcode-14-Longest Common Prefix
+ *
+ * Write a function to find the longest common prefix string amongst an array of strings.
+ *
+ * If there is no common prefix, return an empty string "".
+ *
+ * Example 1:
+ * Input: ["flower","flow","flight"]
+ * Output: "fl"
+ *
+ * Example 2:
+ * Input: ["dog","racecar","car"]
+ * Output: ""
+ * Explanation: There is no common prefix among the input strings.
+ * Note:
+ *
+ * All given inputs are in lowercase letters a-z.
+ */
+public class LongestCommonPrefix {
+    public String longestCommonPrefix(String[] strs) {
+
+        if (strs == null || strs.length < 1) {
+            return "";
+        }
+
+        String result = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(result) != 0) {
+                // 如果result在strs[i]中不存在，则缩短result
+                result = result.substring(0, result.length() - 1);
+            }
+        }
+        return result;
+    }
+}
