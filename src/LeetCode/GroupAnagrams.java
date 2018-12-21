85c75c6b53b3292bc4f6c4d6581a24c00081745c
Junrui Gong <jgong8@hawk.iit.edu>
2018-12-21 14:38:26 +0800

    Group Anagrams

---
+++ src/LeetCode/GroupAnagrams.java
@@ -0,0 +1,93 @@
+package LeetCode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Leetcode-49-Group Anagrams
+ *
+ * Given an array of strings, group anagrams together.
+ *
+ * Example:
+ *
+ * Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
+ * Output:
+ * [
+ *   ["ate","eat","tea"],
+ *   ["nat","tan"],
+ *   ["bat"]
+ * ]
+ * Note:
+ *
+ * All inputs will be in lowercase.
+ * The order of your output does not matter.
+ */
+public class GroupAnagrams {
+
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        List<List<String>> lists = new ArrayList<>();
+
+        if (strs == null || strs.length < 1) {
+            return lists;
+        }
+
+        HashMap<String, Integer> hashMap = new HashMap<>();
+
+        for (int i = 0; i < strs.length; i++) {
+            char[] c = strs[i].toCharArray();
+            Arrays.sort(c);
+            String str = new String(c);
+
+            if (hashMap.containsKey(str)) {
+                // 从lists中取出list并添加新的字符串
+                List<String> list = lists.get(hashMap.get(str));
+                list.add(strs[i]);
+
+            } else {
+                List<String> list = new ArrayList<>();
+                list.add(strs[i]);  // list放sort前的字符串
+                hashMap.put(str, lists.size());  //hashmap放sort之后的字符串
+                lists.add(list);
+            }
+        }
+
+        return lists;
+    }
+
+    public List<List<String>> groupAnagrams2(String[] strs) {
+
+        HashMap<String, List<String>> hashMap = new HashMap<>();
+
+        for (String str : strs) {
+            int[] count = new int[26];
+            for (Character ch : str.toCharArray()) {
+                count[ch - 'a']++;
+            }
+
+            String s = "";
+
+            for (int i = 0; i < count.length; i++) {
+                if (count[i] != 0) {
+                    s += String.valueOf(count[i]) + String.valueOf((char)(i + 'a'));
+                }
+            }
+
+            if (hashMap.containsKey(s)) {
+                List<String> list = hashMap.get(s);
+                list.add(str);
+
+            } else {
+                List<String> list = new ArrayList<>();
+                list.add(str);
+                hashMap.put(s, list);
+            }
+        }
+
+        return new ArrayList<>(hashMap.values());
+
+    }
+}
