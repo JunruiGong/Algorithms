5c3f44e41ce2fcbe9f4fc8f87ea60c5dbb9c9204
Junrui Gong <jgong8@hawk.iit.edu>
2018-12-25 14:56:36 +0800

    Excel Sheet Column Number

---
+++ src/LeetCode/ExcelSheetColumnNumber.java
@@ -0,0 +1,50 @@
+package LeetCode;
+
+import org.junit.Test;
+
+/**
+ * Leetcode-171-Excel Sheet Column Number
+ *
+ * Given a column title as appear in an Excel sheet, return its corresponding column number.
+ *
+ * For example:
+ *
+ *     A -> 1
+ *     B -> 2
+ *     C -> 3
+ *     ...
+ *     Z -> 26
+ *     AA -> 27
+ *     AB -> 28
+ *     ...
+ *
+ * Example 1:
+ * Input: "A"
+ * Output: 1
+ *
+ * Example 2:
+ * Input: "AB"
+ * Output: 28
+ *
+ * Example 3:
+ * Input: "ZY"
+ * Output: 701
+ */
+public class ExcelSheetColumnNumber {
+    public int titleToNumber(String s) {
+        int result = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            result = result * 26 + (s.charAt(i) - 'A' + 1);
+        }
+
+        return result;
+    }
+
+    @Test
+    public void Test(){
+        String s = "A";
+        System.out.println(titleToNumber(s));
+
+    }
+}
