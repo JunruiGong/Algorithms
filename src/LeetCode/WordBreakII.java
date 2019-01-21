421a22c339cf7c7bf81db9325ba700f39819ec95
Junrui Gong <jgong8@hawk.iit.edu>
2019-01-21 05:45:16 -0600

    Word Break II

--- src/LeetCode/WordBreakII.java
+++ src/LeetCode/WordBreakII.java
@@ -1,4 +1,97 @@
 package LeetCode;
 
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Leetcode-140-Word Break II-Hard
+ *
+ * Given a non-empty string s and a dictionary wordDict containing a list of non-empty words,
+ * add spaces in s to construct a sentence where each word is a valid dictionary word. Return all such possible sentences.
+ *
+ * Note:
+ * The same word in the dictionary may be reused multiple times in the segmentation.
+ * You may assume the dictionary does not contain duplicate words.
+ *
+ * Example 1:
+ * Input:
+ * s = "catsanddog"
+ * wordDict = ["cat", "cats", "and", "sand", "dog"]
+ * Output:
+ * [
+ *   "cats and dog",
+ *   "cat sand dog"
+ * ]
+ *
+ * Example 2:
+ * Input:
+ * s = "pineapplepenapple"
+ * wordDict = ["apple", "pen", "applepen", "pine", "pineapple"]
+ * Output:
+ * [
+ *   "pine apple pen apple",
+ *   "pineapple pen apple",
+ *   "pine applepen apple"
+ * ]
+ * Explanation: Note that you are allowed to reuse a dictionary word.
+ *
+ * Example 3:
+ * Input:
+ * s = "catsandog"
+ * wordDict = ["cats", "dog", "sand", "and", "cat"]
+ * Output:
+ * []
+ */
 public class WordBreakII {
+
+    HashMap<Integer, List<String>> map = new HashMap<>();
+
+    public List<String> wordBreak(String s, List<String> wordDict) {
+        return help(s, wordDict, 0);
+    }
+
+    private List<String> help(String s, List<String> wordDict, int start) {
+        if (map.containsKey(start)) {
+            return map.get(start);
+        }
+
+        List<String> result = new ArrayList<>();
+
+        if (start == s.length()) {
+            result.add("");
+        }
+
+        for (int end = start + 1; end <= s.length(); end++) {
+            if (wordDict.contains(s.substring(start, end))) {
+
+                // list里面添加的是单个单词
+                List<String> list = help(s, wordDict, end);
+                System.out.println(list);
+                for (String temp : list) {
+                    // 将list里的单词进行拼接
+                    result.add(s.substring(start, end) + (temp.equals("") ? "" : " ") + temp);
+                    System.out.println(result);
+                }
+            }
+        }
+
+        map.put(start, result);
+        return result;
+    }
+
+    @Test
+    public void test() {
+        String s = "catsanddog";
+        List<String> wordDict = new ArrayList<>();
+        wordDict.add("cat");
+        wordDict.add("cats");
+        wordDict.add("and");
+        wordDict.add("sand");
+        wordDict.add("dog");
+
+        System.out.println(wordBreak(s, wordDict));
+    }
 }
