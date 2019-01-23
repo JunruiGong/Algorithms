299bd064667257c7c8fdd606677f79e28bb86182
Junrui Gong <jgong8@hawk.iit.edu>
2019-01-22 23:15:43 -0600

    Wildcard Matching

---
+++ src/LeetCode/WildcardMatching.java
@@ -0,0 +1,68 @@
+package LeetCode;
+
+/**
+ * Leetcode-44-Wildcard Matching-Hard
+ *
+ * Given an input string (s) and a pattern (p), implement wildcard pattern matching with support for '?' and '*'.
+ *
+ * '?' Matches any single character.
+ * '*' Matches any sequence of characters (including the empty sequence).
+ * The matching should cover the entire input string (not partial).
+ *
+ * Note:
+ * s could be empty and contains only lowercase letters a-z.
+ * p could be empty and contains only lowercase letters a-z, and characters like ? or *.
+ *
+ * Example 1:
+ * Input:
+ * s = "aa"
+ * p = "a"
+ * Output: false
+ * Explanation: "a" does not match the entire string "aa".
+ *
+ * Example 2:
+ * Input:
+ * s = "aa"
+ * p = "*"
+ * Output: true
+ * Explanation: '*' matches any sequence.
+ *
+ * Example 3:
+ * Input:
+ * s = "cb"
+ * p = "?a"
+ * Output: false
+ * Explanation: '?' matches 'c', but the second letter is 'a', which does not match 'b'.
+ *
+ * 与leetcode-10相似
+ */
+public class WildcardMatching {
+    public boolean isMatch(String s, String p) {
+
+        boolean[][] dp = new boolean[s.length() + 1][p.length() + 1];
+        dp[s.length()][p.length()] = true;
+
+        // 将最后一列初始化
+        for (int i = p.length() - 1; i >= 0; i--) {
+            if (p.charAt(i) == '*') {
+                dp[s.length()][i] = true;
+            } else {
+                break;
+            }
+        }
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            for (int j = p.length() - 1; j >= 0; j--) {
+                if (s.charAt(i) == p.charAt(j) || p.charAt(j) == '?') {
+                    dp[i][j] = dp[i + 1][j + 1];
+                } else if (p.charAt(j) == '*') {
+                    dp[i][j] = dp[i + 1][j] || dp[i][j + 1];
+                } else {
+                    dp[i][j] = false;
+                }
+            }
+        }
+
+        return dp[0][0];
+    }
+}
