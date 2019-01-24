428c1d9bfa0c4d5a14352f521ada5897dd9b0475
Junrui Gong <jgong8@hawk.iit.edu>
2019-01-23 23:09:09 -0600

    Linked List Cycle II

---
+++ src/LeetCode/LinkedListCycleII.java
@@ -0,0 +1,87 @@
+package LeetCode;
+
+
+/**
+ * Leetcode-142-Linked List Cycle II-Medium
+ *
+ * Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
+ *
+ * To represent a cycle in the given linked list,
+ * we use an integer pos which represents the position (0-indexed) in the linked list where tail connects to.
+ * If pos is -1, then there is no cycle in the linked list.
+ *
+ * Note: Do not modify the linked list.
+ *
+ * Example 1:
+ *
+ *
+ * Input: head = [3,2,0,-4], pos = 1
+ * Output: tail connects to node index 1
+ * Explanation: There is a cycle in the linked list, where tail connects to the second node.
+ *
+ *
+ * Example 2:
+ *
+ * Input: head = [1,2], pos = 0
+ * Output: tail connects to node index 0
+ * Explanation: There is a cycle in the linked list, where tail connects to the first node.
+ *
+ *
+ * Example 3:
+ *
+ * Input: head = [1], pos = -1
+ * Output: no cycle
+ * Explanation: There is no cycle in the linked list.
+ *
+ *
+ *
+ *
+ * Follow up:
+ * Can you solve it without using extra space?
+ */
+public class LinkedListCycleII {
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+
+            // 找到环的起始点
+            if (fast == slow) {
+                ListNode slow2 = head;
+                while (slow2 != slow) {
+                    slow = slow.next;
+                    slow2 = slow2.next;
+                }
+
+                return slow;
+            }
+        }
+
+        return null;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
